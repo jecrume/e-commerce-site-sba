f1b79367937007655d1a47edb8ae530689b381ac
John Crume <jecrume@gmail.com>
2022-01-14 22:03:47 -0500

    First Commit, completed nav bar

---
+++ public/js/nav.js
@@ -0,0 +1,33 @@
+//Dynamically adds navbar to each page where scripte is included
+
+const createNav = () => {
+    let nav =document.querySelector('.nav-bar');
+
+    nav.innerHTML = `
+    <div class="nav">
+            <img src="img/GoGoGadgets-logo-new.png"  class="brand-logo" alt="">
+        
+            <div class="nav-items">
+
+                <div class="search">
+                    <input type="text" class="search-box" placeholder="Search brands, products, etc.">
+                    <button class="search-btn">Search</button>
+                </div>
+                <a href="#" class="user"><img src="img/user.png" alt=""></a>
+                <a href="#" class="cart"><img src="img/cart.png" alt=""></a>    
+
+            </div>
+        </div>
+        <ul class="links-container">
+            <li class="link-item"><a href="#" class="link">Home</a></li>
+            <li class="link-item"><a href="#" class="link">Computers</a></li>
+            <li class="link-item"><a href="#" class="link">Gaming</a></li>
+            <li class="link-item"><a href="#" class="link">3d Printing</a></li>
+            <li class="link-item"><a href="#" class="link">Toys</a></li>
+            
+        </ul>
+    
+    `;
+} 
+
+createNav();
